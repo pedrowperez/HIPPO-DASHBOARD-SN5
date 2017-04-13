3b7b9b7f4354c726f7cac1fb881e663a75d8f811
GIOVANI DE PAIVA COLOMBAROLLI <giovani.pcolombaroll@senacsp.edu.br>
2017-04-12 21:05:40 -0300

    pagina default

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
-<?php 
-
-	$result = odbc_fetch_array($query);	
-
-	if(!empty($result['idUsuario']) &&
-	   !empty($result['idUsuario'])){
-		
-		$_SESSION['idUsuario'] =
-		$result['idUsuario'];
-		$_SESSION['tipoPerfil'] =
-		$result['tipoPerfil'];
-		$_SESSION['nomeUsuario'] = 
-		$result['nomeUsuario'];
-		
-		header("location:../menu");
-		
-	   }else{
-			$erro = 'Email ou Senha Incorretos';
-	   }	
-}
-	
-include('index.tpl.php');
-
-?>
+<html>
+<head>
+	<meta http-equiv="refresh" content="0; url=auth"/>
+</head>
+</html>
