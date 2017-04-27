459b7f9118ed80752057cb838056fec2053c5b5b
ANDERSON LUCAS DE PAIVA <anderson.lpaiva@senacsp.edu.br>
2017-04-26 21:56:44 -0300

    Novo Usuário versão 1

--- users/index.php
+++ users/index.php
@@ -1,38 +1,127 @@
-<?php 
-	include ('../db/index.php');
-	include ('../auth/controle_de_acesso.php');
+<?php
+include('../db/index.php');
+include('../auth/controle_de_acesso.php');
 
-		if(isset($_REQUEST['acao'])) {
-			switch($acao) {
-				case 'excluir';
-				 if(is_numeric($_GET['id'])) {
-					if(odbc_exec($db, "DELETE FROM
+if(isset($_REQUEST['acao'])){
+	
+	$acao = $_REQUEST['acao'];
+	
+	switch($acao){
+		case 'incluir':
+			include('incluir_usuario_tpl.php');
+			break;
+		case 'excluir':
+			if(is_numeric($_GET['id'])){
+				if($q = odbc_exec($db, "	DELETE FROM 
 										Usuario
-										WHERE
-											idUsuario = {$_GET['id']}")) {
-						$msg = "USUARIO EXCLUIDO COM SUCESSO";
-				 }else {
-					 $erro = "ERRO AO EXCLUIR USUÁRIO"; 
-				 }
-				 
-				break;
+									WHERE
+										idUsuario = {$_GET['id']}")){
+					if(odbc_num_rows($q) > 0){
+						$msg = "Usu&aacute;rio exclu&iacute;do com sucesso";
+					}else{
+						$erro = "Usu&aacute;rio n&atilde;o existe";
+					}
+				}else{
+					$erro = "Erro ao excluir o usu&aacute;rio";
+				}
+			}else{
+				$erro = "ID inv&aacute;lido";
 			}
-		}
-		}else {
+			
 			$q = odbc_exec($db, 'SELECT 
 									idUsuario,
-									loginUsuario, 
-									nomeUsuario, 
-									tipoPerfil, 
+									loginUsuario,
+									nomeUsuario,
+									tipoPerfil,
 									usuarioAtivo
 								FROM
 									Usuario');
-		$i = 0;
-		while($r = odbc_fetch_array($q)) {
-			$usuarios[$i] = $r;
-			$i++;
-		}
+			$i = 0;							
+			while($r = odbc_fetch_array($q)){
+				$usuarios[$i] = $r;
+				$i++;
+			}
+			include('lista_usuario_tpl.php');	
+					
+			break;
+		
+		default:
+			$erro = "A&ccedil;&atilde;o inv&aacute;lida";
+	}
+	
+}else{
 
-		include ('lista_usuarios.tpl.php');
+	//insere novo usuario
+	if(isset($_POST['btnNovoUsuario'])){
+		//trata nome
+		$nome = preg_replace(	"/[^a-zA-Z0-9 ]+/", 
+								"", 
+								$_POST['nome']);
+		//trata email
+		$email_exploded = 
+		explode('@',$_POST['login']);
+		$email_comeco = preg_replace(	"/[^a-z0-9._+-]+/i", "",$email_exploded[0]);
+		$email_fim = preg_replace(	"/[^a-z0-9._+-]+/i", "",$email_exploded[1]);
+		$email = $email_comeco.'@'.$email_fim;
+		
+		//trata senha
+		$password = str_replace('"','',$_POST['senha']);
+		$password = str_replace("'",'',$password);
+		$password = str_replace(';','',$password);
+		
+		//trata perfil
+		$perfil = 	$_POST['perfil'] != 'A' 
+					&& $_POST['perfil'] != 'C' 
+					? 'C' :	$_POST['perfil'];
+		
+		//trata ativo
+		$_POST['ativo'] = !isset($_POST['ativo']) ? 0 : $_POST['ativo'];
+		$ativo = (bool) $_POST['ativo'];
+		$ativo = $ativo === true ? 1 : 0;
+		
+		if(odbc_exec($db, "	INSERT INTO
+								Usuario
+								(loginUsuario,
+								senhaUsuario,
+								nomeUsuario,
+								tipoPerfil,
+								usuarioAtivo)
+							VALUES
+								('$email',
+					HASHBYTES('SHA1','$password'),
+								'$nome',
+								'$perfil',
+								$ativo)")){
+			$msg = "Usu&aacute;rio gravado com sucesso";					
+		}else{
+			$erro = "Erro ao gravar o usu&aacute;rio";
 		}
+	}
+
+	$q = odbc_exec($db, 'SELECT 
+							idUsuario,
+							loginUsuario,
+							nomeUsuario,
+							tipoPerfil,
+							usuarioAtivo
+						FROM
+							Usuario');
+	$i = 0;							
+	while($r = odbc_fetch_array($q)){
+		$usuarios[$i] = $r;
+		$i++;
+	}
+	include('lista_usuarios.tpl.php');
+}
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- users/lista_usuarios.tpl.php
+++ users/lista_usuarios.tpl.php
@@ -2,11 +2,12 @@
 include('../menu/index.head.tpl.php');
 include('../menu/index.body.tpl.php');
 
-?>
+	
+		?>
   <div class="row">
                     <div class="col-lg-12">
                         <h1 class="page-header">
-                            Atualização de Usuarios <small>Overview</small>
+                            Atualização de Usuarios <small> Visão Geral</small>
                         </h1>
                         <ol class="breadcrumb">
                             <li class="active">
@@ -15,6 +16,53 @@ include('../menu/index.body.tpl.php');
                         </ol>
                     </div>
                 </div>
+				<div class="row">
+				<div class="col-lg-12"> 
+				<div class="panel panel-default">
+                        <div class="panel-heading">
+                            Adicionar novo Usuário
+                        </div>
+                        <div class="panel-body">
+                            <div class="row">
+                                <div class="col-lg-12">
+                                    <form method="post" action="../usuario/" role="form">
+                                        <div class="form-group">
+                                            <label>Nome</label>
+                                            <input class="form-control" name="nome" placeholder="NOME">
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Login</label>
+                                            <input class="form-control" name="login" placeholder="LOGIN">
+                                        </div>
+										<div class="form-group">
+                                            <label>Senha</label>
+                                            <input class="form-control" name="senha" placeholder="SENHA">
+                                        </div>
+										 <div class="form-group">
+                                            <label>Tipo de Usuário</label>
+                                            <select name="perfil" class="form-control">
+                                                <option value="A">ADMINISTRADOR</option>
+                                                <option value="C">CLIENTE</option>
+                                            </select>
+                                        </div>
+                                        <div class="form-group">
+                                            <label>Ativo</label>
+                                            <div class="checkbox">
+                                                <label>
+                                                    <input type="checkbox" name="ativo" checked> ON
+                                                </label>
+                                            </div>
+                                        </div>
+	<input type="submit" value="Gravar" class="btn btn-default" name="btnNovoUsuario">	
+                                 
+                                    </form>
+                                </div>
+                  
+                            </div>
+                            <!-- /.row (nested) -->
+                        </div>
+                        <!-- /.panel-body -->
+                    </div></div></div>
  <div class="row">	
 	<div class="col-lg-12">
 <table class="table table-hover table-striped">
